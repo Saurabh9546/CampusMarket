aed183e7e8b770773d09f96dea4a0c8c180734b4
Saurabh Kumar Srivastav <rudrasaurabh666@gmail.com>
2026-09-06 15:36:23 +0530

    Migrate email sending from SMTP to Resend API (Railway blocks outbound SMTP)

--- CampusMarket_Backend/src/main/java/com/campusmarket/backend/auth/service/EmailService.java
+++ CampusMarket_Backend/src/main/java/com/campusmarket/backend/auth/service/EmailService.java
@@ -1,39 +1,53 @@
 package com.campusmarket.backend.auth.service;
 
+import com.resend.Resend;
+import com.resend.services.emails.model.CreateEmailOptions;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.mail.SimpleMailMessage;
-import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
 @Service
 public class EmailService {
 
-    private final JavaMailSender mailSender;
+    private final Resend resend;
     private final String frontendUrl;
+    private final String fromAddress;
 
-    public EmailService(JavaMailSender mailSender, @Value("${app.frontend-url}") String frontendUrl) {
-        this.mailSender = mailSender;
+    public EmailService(
+            @Value("${resend.api-key}") String resendApiKey,
+            @Value("${app.frontend-url}") String frontendUrl,
+            @Value("${resend.from-address}") String fromAddress) {
+        this.resend = new Resend(resendApiKey);
         this.frontendUrl = frontendUrl;
+        this.fromAddress = fromAddress;
     }
 
     public void sendVerificationEmail(String toEmail, String token) {
         String link = frontendUrl + "/verify-email?token=" + token;
-        SimpleMailMessage message = new SimpleMailMessage();
-        message.setTo(toEmail);
-        message.setSubject("Verify your CampusMarket account");
-        message.setText("Welcome to CampusMarket!\n\nPlease verify your email by clicking the link below:\n"
-                + link + "\n\nThis link expires in 24 hours.");
-        mailSender.send(message);
+        String body = "Welcome to CampusMarket!<br><br>Please verify your email by clicking the link below:<br>"
+                + "<a href=\"" + link + "\">" + link + "</a><br><br>This link expires in 24 hours.";
+        send(toEmail, "Verify your CampusMarket account", body);
     }
 
     public void sendPasswordResetEmail(String toEmail, String token) {
         String link = frontendUrl + "/reset-password?token=" + token;
-        SimpleMailMessage message = new SimpleMailMessage();
-        message.setTo(toEmail);
-        message.setSubject("Reset your CampusMarket password");
-        message.setText("We received a request to reset your CampusMarket password.\n\n"
-                + "Click the link below to set a new password:\n" + link
-                + "\n\nThis link expires in 1 hour. If you didn't request this, ignore this email.");
-        mailSender.send(message);
+        String body = "We received a request to reset your CampusMarket password.<br><br>"
+                + "Click the link below to set a new password:<br>"
+                + "<a href=\"" + link + "\">" + link + "</a>"
+                + "<br><br>This link expires in 1 hour. If you didn't request this, ignore this email.";
+        send(toEmail, "Reset your CampusMarket password", body);
+    }
+
+    private void send(String toEmail, String subject, String htmlBody) {
+        CreateEmailOptions params = CreateEmailOptions.builder()
+                .from(fromAddress)
+                .to(toEmail)
+                .subject(subject)
+                .html(htmlBody)
+                .build();
+        try {
+            resend.emails().send(params);
+        } catch (Exception e) {
+            throw new RuntimeException("Failed to send email via Resend", e);
+        }
     }
 }
